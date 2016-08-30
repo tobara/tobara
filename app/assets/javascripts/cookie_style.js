1122414079c0340623995d27a4d284ddc04a675b
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-08-30 11:46:39 -0400

    try try again

--- app/assets/javascripts/cookie_style.js
+++ app/assets/javascripts/cookie_style.js
@@ -1,5 +1,1 @@
-if ( $.cookie('styleCookie') === 'style-light.css') {
-  $('html, body').css('background', '#eeeeee');
-} else if ($.cookie('styleCookie') === 'style.css') {
-  $('html, body').css('background', '#222222');
-}
+
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -29,9 +29,8 @@ Rails.application.config.assets.precompile += %w( jquery.flexslider-min.js )
 Rails.application.config.assets.precompile += %w( viewport-units-buggyfill.js )
 Rails.application.config.assets.precompile += %w( main.js )
 Rails.application.config.assets.precompile += %w( modernizr-2.6.2.min.js )
-Rails.application.config.assets.precompile += %w( cookie_style.js )
+Rails.application.config.assets.precompile += %w( style-light.css )
 Rails.application.config.assets.precompile += %w( jquery.min.js )
 Rails.application.config.assets.precompile += %w( jquery.cookie.js )
-Rails.application.config.assets.precompile += %w( jquery.jscrollpane.css )
-Rails.application.config.assets.precompile += %w( jquery.jscrollpane.min.js )
+
 
