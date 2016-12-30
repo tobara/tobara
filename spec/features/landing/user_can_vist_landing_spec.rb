3bf859bb7650f338ba33e72dfe804ec014b06d3b
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-12-30 12:06:44 -0500

    better tests all passing

--- spec/features/landing/user_can_vist_landing_spec.rb
+++ spec/features/landing/user_can_vist_landing_spec.rb
@@ -7,7 +7,8 @@ feature "The landing page ", %{
   and other information about the candidate
   Acceptance Criteria
   [X] I am able to view information about Timothy OBara
-  [X] I am able to view past projects
+  [X] I am able to view past projects info
+  [X] I am able to view project cut-outs
 
 
 } do
@@ -18,13 +19,32 @@ feature "The landing page ", %{
 
 
     expect(page).to have_content('Extraterrestrial')
+    expect(page).not_to have_content('Who I Am')
   end
 
-  scenario "employer visits site" do
+  scenario "employer wants to view portfolio info" do
     visit root_path
     click_link "See my portfolio"
 
     expect(page).to have_content('DoctorLoans.io')
+    expect(page).to have_content('MatthewsRental.com')
+    expect(page).to have_content('Dance North County')
+    expect(page).to have_content('DrinkBetterBourbon')
+    expect(page).not_to have_content('Ever since I was young')
   end
 
+  scenario "employer wants to view portfolio cut-outs" do
+    visit root_path
+    click_link "See my portfolio"
+
+    expect(page).to have_selector( 'img[alt="DoctorLoans.io Desktop View"]')
+    expect(page).to have_selector( 'img[alt="DoctorLoans.io Mobile View"]')
+    expect(page).to have_selector( 'img[alt="MatthewsRental.com Desktop View"]')
+    expect(page).to have_selector( 'img[alt="MatthewsRental.com Mobile View"]')
+    expect(page).to have_selector( 'img[alt="Dance North County Desktop View"]')
+    expect(page).to have_selector( 'img[alt="Dance North County Mobile View"]')
+    expect(page).to have_selector( 'img[alt="Drink Better Bourbon Desktop View"]')
+    expect(page).to have_selector( 'img[alt="Drink Better Bourbon Mobile View"]')
+    expect(page).not_to have_selector( 'img[alt="Timothy OBara Resume Image"]')
+  end
 end
